59aca3cfe15be90ea7e543db01cca1de623008ba
DerpMarine <joel@ellis.link>
2019-12-29 16:56:53 +0000

    MVP

---
+++ src/app.js
@@ -0,0 +1,66 @@
+const moment = require('moment');
+let cfg = false;
+let clock = document.getElementById('clock');
+let sub = document.getElementById('clock-sub');
+var configureTimeout;
+
+function configure () {
+  shouldConfigure = false;
+  console.log(cfg);
+  cfg.scale = cfg.scale || '1';
+  document.title = cfg.tabTitle;
+  if (cfg.invertColours) {
+    document.body.classList.add('invert');
+  }
+  if (cfg.clockEnabled) {
+    clock.style.display = 'block';
+    updateClock();
+    setInterval(updateClock, 500);
+  }
+  if (cfg.subEnabled) {
+    sub.style.display = 'block';
+    updateSub();
+    setInterval(updateSub, 500);
+  }
+  updScale();
+}
+function updScale () {
+  // let h = (document.getElementById('clock-container').getBoundingClientRect().height / document.documentElement.clientHeight);
+  // let w = (Math.max(document.getElementById('clock-sub').getBoundingClientRect().width, document.getElementById('clock').getBoundingClientRect().width) / document.documentElement.clientWidth);
+
+  // while (h < 0.20 || h > 0.95) {
+  //   console.log(h);
+  //   if (h > 0.95 || w > 1.2) {
+  //     cfg.scale = String(Number(cfg.scale) - 0.1);
+  //   } else if (h < 0.20) {
+  //     cfg.scale = String(Number(cfg.scale) + 0.1);
+  //   }
+
+  //   console.log(cfg.scale);
+  document.body.style.transform = 'scale(' + String(cfg.scale) + ')';
+  //   h = (document.getElementById('clock-container').getBoundingClientRect().height / document.documentElement.clientHeight);
+  //   // w = (Math.max(document.getElementById('clock-sub').getBoundingClientRect().width, document.getElementById('clock').getBoundingClientRect().width) / document.documentElement.clientWidth);
+  //   // w = 1
+  // }
+}
+function updateClock () {
+  clock.textContent = moment().format(cfg.clockFmt);
+}
+function updateSub () {
+  sub.textContent = moment().format(cfg.subFmt);
+}
+browser.storage.sync.get(['tabTitle', 'clockFmt', 'clockEnabled', 'subFmt', 'subEnabled', 'invertColours',
+   'scale'
+]).then((cfgResponse) => {
+  cfg = cfgResponse;
+  if (docReady) { configure(); } else { shouldConfigure = true; }
+});
+let docReady = false;
+let shouldConfigure = false;
+document.addEventListener('DOMContentLoaded', () => {
+  clock = document.getElementById('clock');
+  sub = document.getElementById('clock-sub');
+  docReady = true;
+  document.getElementById('hidden-config-floating').addEventListener('click', () => browser.runtime.openOptionsPage());
+  if (shouldConfigure) { configure(); }
+});
---
+++ src/options.js
@@ -0,0 +1,62 @@
+// import Sortable from 'sortablejs';
+
+// const moment = require('moment');
+console.log('loaded');
+
+const fields = ['tabTitle', 'clockFmt', 'clockEnabled', 'subFmt', 'subEnabled', 'invertColours', 'scale'
+];
+
+const defaultOpts = {
+  tabTitle: 'New Tab',
+  clockFmt: 'LTS',
+  clockEnabled: 'on',
+  subFmt: 'dddd[,] Mo MMMM',
+  subEnabled: 'on',
+  invertColours: '',
+  scale: '1.0'
+};
+const colourPalette = [
+  'ff8c00', 'e81123', 'd13438', 'c30052', 'bf0077', '9a0089', '881798', '744da9',
+  '10893e', '107c10', '018574', '2d7d9a', '0063b1', '6b69d6', '8e8cd8', '8764b8',
+  '038387', '486860', '525e54', '7e735f', '4c4a48', '515c6b', '4a5459'
+];
+
+const saveOptions = e => {
+  e.preventDefault();
+  let t = {};
+  fields.forEach((n) => {
+    t[n] = document.getElementById(n).value || defaultOpts[n] || '';
+  });
+
+  browser.storage.sync.set(t);
+};
+
+const restoreOptions = _ => {
+  browser.storage.sync.get(fields)
+    .then(options => {
+      console.log(options)
+      fields.forEach((n) => {
+        let el = document.getElementById(n);
+        let type = el.getAttribute('type');
+        if (type === 'checkbox') {
+          el.checked = Boolean(options[n] || defaultOpts[n] || '');
+        } else { el.value = options[n] || defaultOpts[n] || ''; }
+      });
+    });
+};
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('form').addEventListener('submit', saveOptions);
+
+// var fragment = new DocumentFragment()
+
+// colourPalette.forEach(function (c) {
+//   var li = document.createElement('option');
+//   li.style.color = c;
+//   li.value = c;
+//   fragment.appendChild(li);
+// });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.getElementById('colour').appendChild(fragment);
+// });
